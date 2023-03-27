01fe1b1773906984c1c32a73a0cfcf9c2eaa0f4f
Ismael <lindoespinoso@gmail.com>
2023-03-27 15:18:24 +0200

    v0.0.2

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 // vue.config.js file to be placed in the root of your repository
 
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/my-projectgreeter-tutorial-starter/'
-      : '/'
-  }
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/amis13' + process.env.CI_PROJECT_NAME + '/greeter-tutorial-starter'
+    : '/'
+}
