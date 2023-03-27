2dde7f54f8a5fcaf015d7a303b10fa0ce208d2d5
Ismael <lindoespinoso@gmail.com>
2023-03-27 16:06:24 +0200

    v.0.0.2

--- src/vue.config.js
+++ src/vue.config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/amis13' + process.env.CI_PROJECT_NAME + '/greeter-tutorial-starter'
+    ? '/' + process.env.CI_PROJECT_NAME + '/'
     : '/'
 }
